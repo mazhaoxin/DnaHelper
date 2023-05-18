27192e149dd0a6e112e621c61e91e8b3660c4cf9
mazhaoxin <34436192+mazhaoxin@users.noreply.github.com>
2023-05-18 22:42:34 +0800

    Fix bugs.

--- gen_dna.py
+++ gen_dna.py
@@ -2,7 +2,15 @@
 # Usage:
 #   1. Copy this script to *.sln located level.
 #   2. Run it.
-#   3. Find *.xll, *.dna and auto_load.vbs in 
+#   3. Find *.xll, *.dna and auto_load.vbs in `./Distribution`.
+# Note:
+#   1. Must involve Excel namespace and rename `Excel.Application` to `XlApp`.
+#   2. Must delare `app`, `wb` and `ws` for `Excel.Application`, `Workbook` and `Worksheet`.
+#   3. Must init them in constructor by
+#       app = new XlApp();
+#       app.Visible = true;
+#       wb = (Workbook)app.Workbooks.Add();
+#       ws = (Worksheet)wb.Worksheets.Add();
 
 
 XLL_PATH = r'E:\Programming\ExcelDNA\ExcelDna64.xll'
@@ -38,13 +46,13 @@ __XML__
 
 REF_TEMPLATE = '<Reference Name="__REF__" />'
 
-XML_TEMPLATE = '''<customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui'>
-  <ribbon onLoad='RibbonLoad'>
+XML_TEMPLATE = '''<customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui' onLoad='RibbonLoad'>
+  <ribbon>
     <tabs>
-      <tab id='CustomTab' label='Custom Task Pane Test'>
-        <group id='SampleGroup' label='CTP Control'>
-          <button id='Button1' label='Show CTP' image='M' size='large' onAction='OnShowCTP' />
-          <button id='Button2' label='Delete CTP' image='M' size='large' onAction='OnDeleteCTP' />
+      <tab id='CustomTab' label='Custom Tab'>
+        <group id='SampleGroup' label='Sample'>
+          <button id='Button1' label='Test-1' imageMso='M' size='large' onAction='Button1Click' />
+          <button id='Button2' label='Test-2' imageMso='M' size='large' onAction='Button2Click' />
         </group >
       </tab>
     </tabs>
@@ -80,10 +88,13 @@ using ExcelDna.Integration.CustomUI;
 // \2''', csharp)
 csharp = re.sub(r'\n}\n', r'\n// }\n', csharp)
 csharp = csharp.replace('public partial class MainForm : Form', f'public class {proj_name}Ribbon : ExcelRibbon')
-csharp = csharp.replace('public MainForm()', 'void RibbonLoad(IRibbonControl sender)')
+csharp = csharp.replace('public MainForm()', 'public void RibbonLoad(IRibbonUI sender)')
 csharp = csharp.replace('app = new XlApp();', 'app = (XlApp)ExcelDnaUtil.Application;')
+csharp = csharp.replace('app.Visible = true;', '// app.Visible = true;')
+csharp = csharp.replace('wb = (Workbook)app.Workbooks.Add();', 'wb = (Workbook)app.ActiveWorkbook;')
+csharp = csharp.replace('ws = (Worksheet)wb.Worksheets.Add();', 'ws = (Worksheet)wb.ActiveSheet;')
 csharp = csharp.replace('InitializeComponent();\n', '// InitializeComponent();\n')
-csharp = csharp.replace('(object sender, EventArgs e)', '(IRibbonControl sender)');
+csharp = re.sub(r'void (.*)\(object sender, EventArgs e\)', r'public void \1(IRibbonControl sender)', csharp)
 
 for action in actions:
     if f'{action}(IRibbonControl ' not in csharp:
@@ -92,10 +103,14 @@ for action in actions:
 # Output ------------------------------------------
 if not os.path.exists(OUT_DIR):
     os.mkdir(OUT_DIR)
-shutil.copyfile(XLL_PATH, OUT_DIR+'/'+proj_name+'.xll')
+xll = OUT_DIR+'/'+proj_name+'.xll'
+if not os.path.exists(xll):
+    shutil.copyfile(XLL_PATH, xll)
 
 dna = DNA_TEMPLATE.replace('__REFERENCE__', ref)
 dna = dna.replace('__CSHARP__', csharp)
 dna = dna.replace('__XML__', xml)
 with open(OUT_DIR+'/'+proj_name+'.dna', 'w', encoding='utf8') as f:
     f.write(dna)
+    
+input('\n    Press ENTER to close ...')
